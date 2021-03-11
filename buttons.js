90761eb2bf198ff7146afbe88606c8c9d826b242
mainal15 <78930501+mainal15@users.noreply.github.com>
2021-03-10 20:36:41 -0500

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -4,3 +4,39 @@ function getName(){
     let name = prompt ("Hi there! What's your name?");
     alert("Welcome to my website," + name);
 }
+
+function project1 () {
+        alert("Project 1 coming soon!");
+}
+
+function project2 () {
+    alert("Project 2 coming soon!");
+}
+
+function project3 () {
+    alert("Project 3 coming soon!");
+}
+
+function project4 () {
+    alert("Project 4 coming soon!");
+}
+
+function project5 () {
+    alert("Project 5 coming soon!");
+}
+
+function project6 () {
+    alert("Project 6 coming soon!");
+}
+
+function project7 () {
+    alert("Project 7 coming soon!");
+}
+
+function project8 () {
+    alert("Project 8 coming soon!");
+}
+
+function project9 () {
+    alert("Project 9 coming soon!");
+}
