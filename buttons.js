ff2da05886ee593ef1f0aa14b673432bd2b756b2
mainal15 <78930501+mainal15@users.noreply.github.com>
2021-03-23 08:39:40 -0400

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -5,8 +5,22 @@ function getName(){
     alert("Welcome to my website," + name);
 }
 
-function project1 () {
-        alert("Project 1 coming soon!");
+//Prompts the user for two users and uses conditional statements to output the larger of the two numbers.
+function largerNumber () {
+        let num1 = prompt("Enter the first number: ");
+        let num1 = prompt("Enter the second number: ");
+        let output = ""
+        id (num1 > num2){
+            output = num1;
+        } else if (num1 == num2){
+            output = "The numbers entered are equal";
+        } else if (num2 > num 1){
+            output = num2;
+        } else {
+            output = "Invalid input"
+        }
+        document.getElementByID("largerNum").innerHTML = output;
+        }
 }
 
 function project2 () {
